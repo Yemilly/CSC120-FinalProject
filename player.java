eb5c9e54f8f26a5044c3e5cae039e93908bbee3f
Yemilly Soto <ysoto@smith.edu>
2023-04-26 23:14:12 -0400

    intro

---
+++ player.java
@@ -0,0 +1,9 @@
+
+
+public class player {
+
+
+
+System.out.println("After a long day, you get home and are ready to eat some good food. Your roommate is an amazing pastry chef, and you know it wrong... but you cant resist the freshly baked chocolate cake!!! They wont notice if you just eat a slice, right?? Well that bliss is short lived when you start to realize you are SHRINKINGGG!!! Navigate the house to find the antidote, and get back to normal!! ");
+
+}
